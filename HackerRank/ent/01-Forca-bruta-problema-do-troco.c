f2d28ffe957427566bffaa70c8ff6222d414b56a
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-08-21 15:55:24 -0300

    Create 01-Forca-bruta-problema-do-troco.c

---
+++ HackerRank/ent/01-Forca-bruta-problema-do-troco.c
@@ -0,0 +1,60 @@
+/*Implemente a solução do problema do troco (conforme apresentado em sala de aula) 
+utilizando a estratégia de força-bruta.
+
+Input Format
+
+A primeira linha deve conter o valor do troco. A segunda linha deve conter a quantidade 
+de moedas, que representará o tamanho do vetor que deverá ser lido logo em sequência.
+
+Constraints
+.
+Output Format
+
+Sequência de moedas que foram retornados como troco.
+
+Sample Input 0
+450
+5
+100
+50
+10
+5
+1
+
+Sample Output 0
+5
+
+Sample Input 1
+378
+6
+100
+50
+25
+10
+5
+1
+
+Sample Output 1
+8*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void troco(int valor)
+{
+
+}
+
+int main(void)
+{
+    int valor;
+
+
+    scanf("%d",& valor);
+
+    troco(valor);
+    
+
+    return 0;
+}

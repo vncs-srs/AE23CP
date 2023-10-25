fd862210d02ca21933527d92901030c7c35fb0d8
Vinicius Soares <vsoares446@gmail.com>
2023-10-25 17:48:07 +0000

    Aula: Grafos 1

---
+++ 12-Grafos-1/GrafoLA.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+#include "GrafoLA.h"
+
+/*
+Na implementação deste código, é considerado que o grafo lido sempre será não direcionado
+*/
+
+struct GrafoLA{
+   int V; // número de vértices
+   int A; // número de arestas
+   Lista **adj;
+};
+
+
+static Lista** iniciar_LA(int n){
+    int i;
+    Lista **adj = (Lista**) malloc(n * sizeof(Lista*));
+
+    for (i = 0; i < n; i++)
+        adj[i] = criar_lista();
+
+    return adj;
+}
+
+
+GrafoLA* iniciar_grafoLA(int v){
+    GrafoLA* G = (GrafoLA*) malloc(sizeof(GrafoLA));
+
+    G->V = v;
+    G->A = 0;
+    G->adj = iniciar_LA(G->V);
+
+    return G;
+}
+
+
+int aresta_existeLA(GrafoLA* G, int v1, int v2){
+    if (G!= NULL)
+        return procurar(v2, G->adj[v1]);
+
+    return 0;
+}
+
+
+void inserir_arestaLA(GrafoLA* G, int v1, int v2){
+    if (!aresta_existeLA(G, v1, v2)){
+        inserir_na_lista(v2, G->adj[v1]);
+        inserir_na_lista(v1, G->adj[v2]);
+        G->A++;
+    }
+}
+
+
+void remover_arestaLA(GrafoLA* G, int v1, int v2){
+    if (aresta_existeLA(G, v1, v2)){
+        remover_na_lista(v2, G->adj[v1]);
+        remover_na_lista(v1, G->adj[v2]);
+        G->A--;
+
+    }
+}
+
+
+void imprimir_arestasLA(GrafoLA* G){
+    int i;
+
+    if (G != NULL)
+        for (i = 0; i < G->V; i++)
+            imprimir_lista(G->adj[i]);
+}
+
+
+void liberarGLA(GrafoLA* G){
+    int i;
+
+    if (G != NULL){
+        for (i = 0; i < G->V; i++)
+            liberar_lista(G->adj[i]);
+
+        free(G);
+    }
+}
